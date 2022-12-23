5317079c3cb2b86b96b64fe0d47f6b4f4a0b7713
drshn-k <110783201+drshn-k@users.noreply.github.com>
2022-12-23 23:29:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Started express server at port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Started express server at ${port}`);
 });
