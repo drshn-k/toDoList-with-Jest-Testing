adf415496e5a0ce2c07f2590ba7ab97d1bbade95
drshn-k <dkasar258@gmail.com>
2022-12-23 12:46:21 +0530

    try_14

--- models/index.js
+++ models/index.js
@@ -13,7 +13,12 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database, 
+    config.username, 
+    config.password, 
+    config
+    );
 }
 
 fs
